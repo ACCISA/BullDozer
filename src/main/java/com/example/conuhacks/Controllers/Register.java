23e73cc8a7bd1e65ee2faf92438b634b33281f03
Romullus <39713625+ACCISA@users.noreply.github.com>
2023-01-24 02:30:18 -0500

    Error

--- src/main/java/com/example/conuhacks/Controllers/Register.java
+++ src/main/java/com/example/conuhacks/Controllers/Register.java
@@ -6,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
@@ -13,6 +15,7 @@ import javafx.scene.layout.StackPane;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -45,29 +48,39 @@ public class Register implements Initializable {
     @FXML
     private AnchorPane registerAnchorPane;
 
-
+    @FXML
+    private StackPane stackP;
 
     @FXML
+    private JFXDialogLayout dialogLayout;
+
+    private ArrayList<JFXDialog> dialogsArr = new ArrayList<>();
+    @FXML
     void actionCreateAccount(ActionEvent event) {
+        if (!(dialogsArr.size()==0)) {
+            for (int i = 0; i < dialogsArr.size(); i++) {
+                dialogsArr.get(i).close();
+                System.out.println("closed");
+            }
+        }
         if (!(Utils.hasValidValues(new String[]{inputUsername.getText(),inputConfirmPassword.getText(),inputPassword.getText()}))){
             dialogInvalidValues.show();
             return;
         }
         if (!(Utils.hasSamePassword(inputPassword.getText(),inputConfirmPassword.getText()))){
-            StackPane stackP = new StackPane();
-            javafx.scene.layout.AnchorPane.setTopAnchor(stackP,20.0);
-            javafx.scene.layout.AnchorPane.setLeftAnchor(stackP, 20.0);
-            javafx.scene.layout.AnchorPane.setRightAnchor(stackP, 20.0);
-            javafx.scene.layout.AnchorPane.setBottomAnchor(stackP, 20.0);
-            registerAnchorPane.getChildren().add(stackP);
-            JFXDialogLayout jfxDialogLayout = new JFXDialogLayout();
+
+//            registerAnchorPane.getChildren().add(stackP);
+//            JFXDialogLayout jfxDialogLayout = new JFXDialogLayout();
             Parent parent;
             try{
                 URL url = new File("src/main/resources/com/example/conuhacks/Dialogs/dialog-invalid-values.fxml").toURI().toURL();
                 parent = FXMLLoader.load(url);
-                jfxDialogLayout.getChildren().add(parent);
-                JFXDialog jfxDialog = new JFXDialog(stackP,jfxDialogLayout, JFXDialog.DialogTransition.LEFT, true);
+                dialogLayout.getChildren().add(parent);
+                JFXDialog jfxDialog = new JFXDialog(root,dialogLayout, JFXDialog.DialogTransition.NONE, true);
                 jfxDialog.show();
+                dialogsArr.add(jfxDialog);
+
+
 
             } catch (IOException e){
                 e.printStackTrace();
@@ -81,17 +94,17 @@ public class Register implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        dialog.setTransitionType(JFXDialog.DialogTransition.LEFT);
-        dialog.setDialogContainer(root);
-        continueBtn.setOnAction(actionEvent -> {
-            dialog.close();
-        });
-
-        dialogInvalidValues.setTransitionType(JFXDialog.DialogTransition.LEFT);
-        dialogInvalidValues.setDialogContainer(root);
-        continueBtn.setOnAction(actionEvent -> {
-            dialogInvalidValues.close();
-        });
+//        dialog.setTransitionType(JFXDialog.DialogTransition.LEFT);
+//        dialog.setDialogContainer(root);
+//        continueBtn.setOnAction(actionEvent -> {
+//            dialog.close();
+//        });
+//
+//        dialogInvalidValues.setTransitionType(JFXDialog.DialogTransition.LEFT);
+//        dialogInvalidValues.setDialogContainer(root);
+//        continueBtn.setOnAction(actionEvent -> {
+//            dialogInvalidValues.close();
+//        });
     }
 }
 
